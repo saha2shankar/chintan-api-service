6eeb551eb6abc997524dd1baacea2b0b8434286a
harishankar <sahharishankar11@gmail.com>
2025-02-22 13:57:47 +0545

    inital commit project created

---
+++ src/main/java/com/chintan/ChintanApiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.chintan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChintanApiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChintanApiServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chintan/ChintanApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chintan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChintanApiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
