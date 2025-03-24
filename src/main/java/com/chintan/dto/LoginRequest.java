159bb1e68f65c12e68e8fefb34b887d516ae716b
harishankar <sahharishankar11@gmail.com>
2025-03-24 15:38:56 +0545

    dating add update

---
+++ src/main/java/com/chintan/dto/LoginRequest.java
@@ -0,0 +1,11 @@
+package com.chintan.dto;
+
+import lombok.Data;
+
+@Data
+public class LoginRequest {
+	
+	private String email;
+	private String password;
+
+}
--- src/main/java/com/chintan/repository/UserRepository.java
+++ src/main/java/com/chintan/repository/UserRepository.java
@@ -9,4 +9,6 @@ public interface UserRepository extends JpaRepository<User, Integer> {
 
 	Boolean existsByEmail(String email);
 
+	User findByEmail(String email);
+
 }
